39f4432cc0a2640a88aaf206ac24a79c3f905610
Juan Camilo <camilork25@gmail.com>
2022-02-05 18:23:44 -0500

    :wrench: Remove tmp change

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,7 +6,7 @@ import { CreateNewUserDTO } from './dtos/user.dto';
 import { UsersService } from './user.service';
 
 @ApiTags('Users')
-//@Auth()
+@Auth()
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
